5e496f9b4dfc53187e83f0fd406ba4df49b81be6
Pive0528 <pive0528@gmail.com>
2024-06-24 16:28:52 +0900

    replace 활용(ã문자열ë êµ문자열 교체)

--- src/main/java/org/koreait/Calc.java
+++ src/main/java/org/koreait/Calc.java
@@ -1,18 +1,23 @@
 package org.koreait;
 
+import java.util.Arrays;
+
 public class Calc {
     public static int run(String exp) {
-//        if (exp.equals("2 + 1")){
-//            return 3;
-//        }
-//        else if (exp.equals("2 + 2")){
-//            return 4;
-//        }
-        String[] bits = exp.split(" \\- ");
+
+        exp = exp.replaceAll("- ", "+ -");  // replace<< 문자열 바꾸기
+
+        String[] bits = exp.split(" \\+ ");
 
         int a = Integer.parseInt(bits[0]);
         int b = Integer.parseInt(bits[1]);
+        int c = 0;
 
-        return a-b;
+        if (bits.length > 2) {
+            c = Integer.parseInt(bits[2]);
+        }
+        return a+b+c;
+
+        // throw new RuntimeException("해석 불가 : 올바른 계산식이 아님");
     }
 }
--- src/test/java/org/koreait/CalcTests.java
+++ src/test/java/org/koreait/CalcTests.java
@@ -29,9 +29,34 @@ public class CalcTests {
     void t4() {
         assertThat(Calc.run("1000 + 200")).isEqualTo(1200);
     }
+
     @Test
     @DisplayName("50 - 30 == 20")   // 이름임. 별상관 ㄴ
     void t5() {
         assertThat(Calc.run("50 - 30")).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("100 - 20 == 80")   // 이름임. 별상관 ㄴ
+    void t6() {
+        assertThat(Calc.run("100 - 20")).isEqualTo(80);
+    }
+
+    @Test
+    @DisplayName("10 + 20 + 30 == 60")   // 이름임. 별상관 ㄴ
+    void t7() {
+        assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
+    }
+
+    @Test
+    @DisplayName("10 - 20 + 30 == 20")   // 이름임. 별상관 ㄴ
+    void t8() {
+        assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10")   // 이름임. 별상관 ㄴ
+    void t9() {
+        assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
     }
 }
